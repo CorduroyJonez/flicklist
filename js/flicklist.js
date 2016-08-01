6067433465d12dcea207cddbc73ba1d958b08f76
Jamin Roberts <jaminfitt@gmail.com>
2016-08-01 22:26:37 +0000

    finish Flicklist 1 studio

--- js/flicklist.js
+++ js/flicklist.js
@@ -1,5 +1,5 @@
 
-
+//two empty arrays within javascript object
 var model = {
   watchlistItems: [],
   browseItems: []
@@ -8,7 +8,7 @@ var model = {
 
 var api = {
   root: "https://api.themoviedb.org/3",
-  token: "TODO" // TODO 0 put your api key here
+  token: "9b45dd869a208f34fd28ec978dd94f50" // TODO 0 put your api key here (DONE)
 }
 
 
@@ -27,9 +27,13 @@ function discoverMovies(callback) {
 			console.log("We got a response from The Movie DB!");
 			console.log(response);
 			
-			// TODO 2
+			// TODO 2 (DONE)
 			// update the model, setting its .browseItems property equal to the movies we recieved in the response
-			
+			// when response comes back from ajax request, console.log 'response' (data that came back from API as a Javascript object) to find that movies reside in .results
+			//results itself is an array of objects (in this case the movies)
+			model.browseItems = response.results;
+			console.log("Here is the model:");
+			console.log(model);
 			// invoke the callback function that was passed in. 
 			callback();
 		}
@@ -42,22 +46,40 @@ function discoverMovies(callback) {
  * re-renders the page with new content, based on the current state of the model
  */
 function render() {
-  // TODO 7
+  // TODO 7 (DONE)
   // clear everything from both lists
-  
-  // TODO 6
-  // for each movie on the user's watchlist, insert a list item into the <ul> in the watchlist section
-  
+  $("#section-watchlist ul").empty();
+  $("#section-browse ul").empty();
+  // TODO 6 (DONE)
+  // for each movie on the user's watchlist, insert a list item into the <ul> in the watchlist section - from wathclistItems array in the model
+	model.watchlistItems.forEach(function(movie) {
+		var itemView = $("<li></li>").text(movie.original_title);
+		$("#section-watchlist ul").append(itemView);
+	});
   // for each movie on the current browse list, 
-  model.browseItems.forEach(function(movie) {
-		// TODO 3
+	model.browseItems.forEach(function(movie) {
+		// TODO 3 (DONE)
 		// insert a list item into the <ul> in the browse section
-		
-		// TODO 4
+		//.text(movie.whatever) because we called it movie in function call**
+		var title = $("<p></p>").text(movie.original_title);
+		//studio asked to wrap paragraph in li tag
+
+		// TODO 4 & 5(DONE)
 		// the list item should include a button that says "Add to Watchlist"
+		var button = $("<button></button>").text("Add to Watchlist").click(function() {
+			model.watchlistItems.push(movie)
+			render();
+			console.log("click!");
+			console.log(model);
+		});
 		
-		// TODO 5
+		var itemView = $("<li></li>").append(title).append(button);
+		
+		$("#section-browse ul").append(itemView);
+		
+		// TODO 5 (DONE-added to 4)
 		// when the button is clicked, this movie should be added to the model's watchlist and render() should be called again
+		
   });
   
 }
